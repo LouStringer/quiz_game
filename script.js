59ca409a5051231c35acf8e0f92af6a9960679ee
Lou Stringer <louise.stringer@gmail.com>
2018-03-10 11:42:50 +0000

    initial commit with instructions

---
+++ script.js
@@ -0,0 +1,11 @@
+Instructions from Complete JavaScript by Jonas Schmedtmann
+1. build a function constructor containing
+    - question
+    - array/object of answers
+    - correct answer (number)
+2. create some questions using constructor
+3. store all the questions in an array
+4. randomly select question, log it to console with answers (use a method)
+5. use prompt to ask for answer - input number of answer chosen
+6. check if answer is correct and console.log if correct or not (method)
+7. make this a module
